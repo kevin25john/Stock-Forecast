cf729162d7a76c2fc556610d1f2952465cae7738
kevin25john <kevin25_john@yahoo.co.in>
2017-03-21 02:03:39 +0530

    Add files via upload

--- stockprediction/stock.py
+++ stockprediction/stock.py
@@ -12,12 +12,12 @@ import yahoo_finance
 from yahoo_finance import Share
 import pandas as pd
 
+#from pandas_datareader import data
 
 name= input("enter the stock keyword  ")
 date_start = input("enter start date in the order YY-MM-DD  ")
 date_end = input("enter end date in the order YY-MM-DD  ")
 symbol = Share(name)
-#print(symbol.get_open())
 google_data = symbol.get_historical(date_start, date_end)
 google_df = pd.DataFrame(google_data)
 
@@ -32,17 +32,18 @@ dates = []
 prices = []
 
 model = SVR(cache_size=7000)
-
+#name = 'SBIN.NS.csv'
 print("hello")
 
 def get_data(name):
     with open(name, 'r') as csvfile:
         csvFileReader = csv.reader(csvfile)
         next(csvFileReader)
-        for row in csvFileReader:
+        for row in reversed(list(csvFileReader)):
             dates.append(int(row[0].split('-')[0]))
             print(dates)
             prices.append(float(row[1]))
+            print(prices)
         return
   
   #
@@ -51,28 +52,29 @@ def get_data(name):
 def predict_prices(dates, prices, x):
     dates = np.reshape(dates, (len(dates), 1))  # @UndefinedVariable
     
-    svr_lin = SVR(kernel='linear', C=1e3)
-    svr_poly = SVR(kernel = 'poly', C=1e3 , degree = 2 )
+    #svr_lin = SVR(kernel='linear', C=1e3)
+    #svr_poly = SVR(kernel = 'poly', C=1e3 , degree = 2 )
     svr_rbf = SVR(kernel = 'rbf', C=1e3 , gamma = 0.1)
-    svr_lin.fit(dates, prices)    
-    svr_poly.fit(dates, prices)
+    #svr_lin.fit(dates, prices)    
+    #svr_poly.fit(dates, prices)
     svr_rbf.fit(dates, prices)
     
     plt.scatter(dates, prices, color='black', label = 'data')
     plt.plot(dates, svr_rbf.predict(dates), color= 'red', label ='RBF model')
-    plt.plot(dates, svr_lin.predict(dates), color= 'green', label ='linear model')
-    plt.plot(dates, svr_poly.predict(dates), color= 'blue', label ='polynomial model')
+   # plt.plot(dates, svr_lin.predict(dates), color= 'green', label ='linear model')
+    #plt.plot(dates, svr_poly.predict(dates), color= 'blue', label ='polynomial model')
     plt.xlabel('Dates')
     plt.ylabel('Price')
     plt.title('Support Vector Regresion')
     plt.show()
     
-    return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
+    return svr_rbf.predict(x)[0]
+# ,svr_lin.predict(x)[0], svr_poly.predict(x)[0]
 
-data_name = name + ".csv"
+data_name = name + '.csv'
 
 get_data(data_name)
 
-predicted_price =   predict_prices(dates, prices, 26)
+predicted_price =   predict_prices(dates, prices, 0)
 print (predicted_price)
 print ("hello")
